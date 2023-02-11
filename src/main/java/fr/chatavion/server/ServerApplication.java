bc9f5d4b30e98b57002a6ed63d4e064ff37a122d
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2023-02-11 14:16:28 +0100

    feat: create community

--- src/main/java/fr/chatavion/server/ServerApplication.java
+++ src/main/java/fr/chatavion/server/ServerApplication.java
@@ -1,8 +1,11 @@
 package fr.chatavion.server;
 
 import fr.chatavion.server.dns.MockDNS;
+import fr.chatavion.server.dns.util.Community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -11,5 +14,18 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 
         new MockDNS(53).start();
+
+        console();
 	}
+
+    public static void console() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            var input = scanner.next();
+            if(input.startsWith("community")) {
+                var communityName = scanner.next();
+                Community.createCommunity(communityName);
+            }
+        }
+    }
 }
