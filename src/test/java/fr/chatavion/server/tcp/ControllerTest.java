8fc823c34c95f046b743bfae292d08f7a57056e9
Léo Buzelin <61974705+Minibuz@users.noreply.github.com>
2023-02-18 14:31:49 +0100

    feat: http TU

--- src/test/java/fr/chatavion/server/tcp/ControllerTest.java
+++ src/test/java/fr/chatavion/server/tcp/ControllerTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,8 +32,36 @@ public class ControllerTest {
     }
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void defaultMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("It works")));
+    }
+
+    @Test
+    public void returnHistory() throws Exception {
+        this.mockMvc.perform(get("/history/test/0?amount=1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":0,\"user\":\"Leo\",\"message\":\"Message1\"}]"));
+    }
+
+    @Test
+    public void validateCommunityTrue() throws Exception {
+        this.mockMvc.perform(get("/community/test")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Test
+    public void validateCommunityFalse() throws Exception {
+        this.mockMvc.perform(get("/community/testt")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("false"));
+    }
+
+    @Test
+    public void sendMessage() throws Exception {
+        this.mockMvc.perform(
+                post("/message/test").contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"username\": \"leo\", \"message\": \"test\"}"))
+                        .andDo(print())
+                        .andExpect(status().isCreated())
+                        .andExpect(content().string("true"));
     }
 }
