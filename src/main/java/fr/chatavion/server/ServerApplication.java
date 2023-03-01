068c6a4c55c9deefee01b5c9ef31bacaf336fc02
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2023-03-01 10:52:05 +0100

    doc: ServerApplication doc

--- src/main/java/fr/chatavion/server/ServerApplication.java
+++ src/main/java/fr/chatavion/server/ServerApplication.java
@@ -5,6 +5,14 @@ import fr.chatavion.server.networking.dns.MockDNS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class represents the main entry point for the server application.
+ * It contains the main method that starts the Spring Boot application
+ * and also starts a mock DNS server and a console for user input.
+ * <br>
+ * To use this class, simply run the main method.
+ * The Spring Boot application, mock DNS server, and console will all start up.
+ */
 @SpringBootApplication
 public class ServerApplication {
 
